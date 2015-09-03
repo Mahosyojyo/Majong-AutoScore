8bc4b34f2deb0e70d310d888c9454fd1d3cc7614
Vespa314 <kylensherlock@gmail.com>
2015-09-03 15:18:39 +0800

    【js】代码排版

--- JS/Majong.js
+++ JS/Majong.js
@@ -1,13 +1,13 @@
 var playerName = ['Aさん', 'Bさん', 'Cさん', 'Dさん'];
 var PointLeft = [25000, 25000, 25000, 25000];
-var jushu = 1;//局数
-var changfeng = '东';//场风
-var benchang = 0;//本场数
+var jushu = 1; //局数
+var changfeng = '东'; //场风
+var benchang = 0; //本场数
 
-var rong_flag = [false,false,false,false];
-var dianpao_flag = [false,false,false,false];
+var rong_flag = [false, false, false, false];
+var dianpao_flag = [false, false, false, false];
 
-var mainView = 0;
+var mainView = 0; //点差模式下主视角
 
 $(document).ready(
     function () {
@@ -16,10 +16,25 @@ $(document).ready(
 
         $('#myonoffswitch').change(ChangePointShowMode);
         ShowPoint(false);
-        
-        UpdateGameProcess();
+
+        UpdateAllView();
     }
 );
+
+function UpdateAllView() {
+    ChangePointShowMode(); //更新点差显示模式
+    UpdateGameProcess(); //更新右上角进度
+    UpdateFengwei(); //更新风位
+    UpdateRank(); //更新顺位
+}
+
+function $q(pattern, idx) { //jQuery辅助函数
+    return $(pattern + ":eq(" + idx + ')');
+}
+
+function UpdateRank() {
+
+}
 
 function ShowPoint(DiffMode) {
     for (var i = 0; i < 4; i++) {
@@ -31,7 +46,20 @@ function ShowPoint(DiffMode) {
     }
 }
 
+function UpdateFengwei() //更新风位
+{
+    $q('.playerpos', (jushu + 3) % 4).text('东');
+    $q('.playerpos', (jushu + 4) % 4).text('南');
+    $q('.playerpos', (jushu + 5) % 4).text('西');
+    $q('.playerpos', (jushu + 6) % 4).text('北');
+
+}
+
 function ChangePointShowMode() {
+    $('.playerpos').not(function (id) {
+        return id == mainView
+    }).removeClass('mainviewcl');
+    $q('.playerpos', mainView).addClass('mainviewcl');
     ShowPoint(!$("#myonoffswitch").is(':checked'));
 }
 
@@ -50,11 +78,10 @@ function UpdateUserName() {
     }
 }
 
-function UpdateGameProcess()
-{
-    var chn = ['一','二','三','四'];
-    $("#changkuang").text(changfeng+chn[jushu-1]+'局');
-    $("#benchangshu").text(benchang+'本场');
+function UpdateGameProcess() {
+    var chn = ['一', '二', '三', '四'];
+    $("#changkuang").text(changfeng + chn[jushu - 1] + '局');
+    $("#benchangshu").text(benchang + '本场');
 }
 
 //随机换座位
@@ -81,48 +108,39 @@ function make_random_position() {
     UpdateUserName();
 }
 
-function rong_click(idx)
-{
+function rong_click(idx) {
     rong_flag[idx] = !rong_flag[idx];
-    if(rong_flag[idx] == false)
-    {
-        $("#player"+idx+"_rong")[0].value = "胡牌";
-        $("#player"+idx+"_rong").removeClass('t_btn_click');
-    }
-    else{
-        $("#player"+idx+"_rong")[0].value = "取消";
-        $("#player"+idx+"_rong").addClass('t_btn_click');
-        if(dianpao_flag[idx] == true)
-        {
-            $("#player"+idx+"_dianpao")[0].value = "点炮";
-            $("#player"+idx+"_dianpao").removeClass('t_btn_click');
+    if (rong_flag[idx] == false) {
+        $("#player" + idx + "_rong")[0].value = "胡牌";
+        $("#player" + idx + "_rong").removeClass('t_btn_click');
+    } else {
+        $("#player" + idx + "_rong")[0].value = "取消";
+        $("#player" + idx + "_rong").addClass('t_btn_click');
+        if (dianpao_flag[idx] == true) {
+            $("#player" + idx + "_dianpao")[0].value = "点炮";
+            $("#player" + idx + "_dianpao").removeClass('t_btn_click');
             dianpao_flag[idx] = false;
         }
     }
 }
 
-function dianpao_click(idx)
-{
+function dianpao_click(idx) {
     dianpao_flag[idx] = !dianpao_flag[idx];
-    if(dianpao_flag[idx] == false)
-    {
-        $("#player"+idx+"_dianpao")[0].value = "点炮";
-        $("#player"+idx+"_dianpao").removeClass('t_btn_click');
-    }
-    else{
-        $("#player"+idx+"_dianpao")[0].value = "取消";
-        $("#player"+idx+"_dianpao").addClass('t_btn_click');
-        if(rong_flag[idx] == true)
-        {
-            $("#player"+idx+"_rong")[0].value = "胡牌";
-            $("#player"+idx+"_rong").removeClass('t_btn_click');
+    if (dianpao_flag[idx] == false) {
+        $("#player" + idx + "_dianpao")[0].value = "点炮";
+        $("#player" + idx + "_dianpao").removeClass('t_btn_click');
+    } else {
+        $("#player" + idx + "_dianpao")[0].value = "取消";
+        $("#player" + idx + "_dianpao").addClass('t_btn_click');
+        if (rong_flag[idx] == true) {
+            $("#player" + idx + "_rong")[0].value = "胡牌";
+            $("#player" + idx + "_rong").removeClass('t_btn_click');
             rong_flag[idx] = false;
         }
     }
 }
 
-function changeView(idx)
-{
+function changeView(idx) {
     mainView = idx;
     ChangePointShowMode();
 }
