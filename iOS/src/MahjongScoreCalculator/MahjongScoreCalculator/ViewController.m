e0aafdf70e9d01cb1a67629ab1ae785195554820
fredfx <fredfx@FSdeMacBook-Pro.local>
2016-01-02 22:33:41 +0800

    开坑

---
+++ iOS/src/MahjongScoreCalculator/MahjongScoreCalculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MahjongScoreCalculator
+//
+//  Created by fredfx on 16/1/2.
+//  Copyright © 2016年 fredfx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
