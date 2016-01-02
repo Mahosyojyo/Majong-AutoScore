47a4ffb09dd3c2734bb96177785bf1b14f36f68e
fredfx <fredfx@FSdeMacBook-Pro.local>
2016-01-02 22:37:45 +0800

    create folder

---
+++ iOS/src/MahjongScoreCalculator/MahjongScoreCalculator/GameCore/MMSCGame.h
@@ -0,0 +1,13 @@
+//
+//  MMSCGame.h
+//  MahjongScoreCalculator
+//
+//  Created by fredfx on 16/1/2.
+//  Copyright © 2016年 fredfx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMSCGame : NSObject
+
+@end
---
+++ iOS/src/MahjongScoreCalculator/MahjongScoreCalculator/GameCore/MMSCGame.m
@@ -0,0 +1,13 @@
+//
+//  MMSCGame.m
+//  MahjongScoreCalculator
+//
+//  Created by fredfx on 16/1/2.
+//  Copyright © 2016年 fredfx. All rights reserved.
+//
+
+#import "MMSCGame.h"
+
+@implementation MMSCGame
+
+@end
